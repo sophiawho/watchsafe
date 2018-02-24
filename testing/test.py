cf8eb496614309a015ce09c0b66eac123eb5240a
rockleeisthegoat <yu.t.hu@mail.mcgill.ca>
2018-02-24 00:20:49 -0500

    Added photo of a girl

--- testing/test.py
+++ testing/test.py
@@ -1,8 +1,11 @@
 from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+import json
 
 app = ClarifaiApp(api_key='f3fa183a15bc4dfba346f09f93b7784c')
 model = app.models.get("moderation")
 
 #predict 
-print model.predict_by_url(url="https://samples.clarifai.com/moderation.jpg")
-
+#print model.predict("girl.jpeg")
+image = ClImage(file_obj=open('girl.jpeg', 'rb'))
+print(model.predict([image]))
