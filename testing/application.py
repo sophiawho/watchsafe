0e127b4a2282343e96ea07513d288e2b62fd6142
Jeffrey Huang <jeff.huang62@gmail.com>
2018-02-24 11:34:33 -0500

    "Clean up folder, update index.html"

--- testing/application.py
+++ testing/application.py
@@ -1,28 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask_cors import CORS
 import jsonify
 
 application = Flask(__name__)
 application.debug = True
+CORS(application)
 
 
 @application.route("/")
 def hello():
-    return "Hello World"
-
+    # return "Hello World"
+    content = open('index.html').read()
+    return Response(content, mimetype="text/html")
 
 # @application.route("/callback", methods={'GET'})
 # def myFunc():
 #     return
+
+@application.route("/test", methods=['POST'])
+def whee():
+    return "success"
 
 
 @application.route("/callback", methods=['POST'])
 def callback():
     data = request.json
     print(data)
-    if not(data['data']):
+    if not (data['data']):
         return 'success'
     else:
         return 'love'
+
 
 if __name__ == "__main__":
     application.run()
